11746a94b874167be3b1510dc5d9d00faadbd08e
Alan Rafael Cortez Garcia <acortez@vivipost.com>
2026-03-14 17:34:06 -0600

    Mejoras

--- src/app/modules/auth/components/home-components/products/products.component.ts
+++ src/app/modules/auth/components/home-components/products/products.component.ts
@@ -1,16 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { LucideAngularModule, Check, Cpu, Cloud, Database } from 'lucide-angular';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [
+    CommonModule, 
+    TranslateModule,
+    LucideAngularModule
+  ],
   encapsulation: ViewEncapsulation.None, // ⬅️ CLAVE
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+
+  Check = Check;
+  Cpu = Cpu;
+  Cloud = Cloud;
+  Database = Database;
 
   constructor() { }
 
